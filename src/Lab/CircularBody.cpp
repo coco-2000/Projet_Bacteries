6328ba93e8aba69b5c2015e309b6b16606ba1396
Emma Boehly <emma.boehly@epfl.ch>
2020-03-15 16:52:09 +0100

    Commentaire des fonctions

--- src/Lab/CircularBody.cpp
+++ src/Lab/CircularBody.cpp
@@ -3,74 +3,81 @@
 #include <iostream>
 
 /**
- * @brief CircularBody::CircularBody
- * @param position
- * @param radius
+ * @brief CircularBody::CircularBody Constructeur
+ * @param position Coordonnées de la position du CircularBody
+ * @param radius Rayon du CircularBody
  */
 CircularBody::CircularBody(Vec2d position, double radius)
     : position(position), radius(radius) {}
+
 /**
- * @brief CircularBody::CircularBody
- * @param other
+ * @brief CircularBody::CircularBody Constructeur de copie
+ * @param other CircularBody à copier
  */
 CircularBody::CircularBody(const CircularBody& other)
 {
     copie(other);
 }
+
 /**
- * @brief CircularBody::getPosition
- * @return position actuelle du corps
+ * @brief CircularBody::getPosition Accesseur de position
+ * @return Position actuelle de l'instance courante
  */
 const Vec2d& CircularBody::getPosition() const
 {
     return position;
 }
+
 /**
- * @brief CircularBody::getRadius
- * @return
+ * @brief CircularBody::getRadius Accesseur de rayon
+ * @return Rayon actuel de l'instance courante
  */
 double CircularBody::getRadius() const
 {
     return radius;
 }
+
 /**
- * @brief CircularBody::setPosition
- * @param pos
+ * @brief CircularBody::setPosition Manipulateur de position
+ * @param pos Coordonnées de la nouvelle position à assigner à l'instance courante
  */
 void CircularBody::setPosition(const Vec2d& pos)
 {
     position = pos;
 }
+
 /**
- * @brief CircularBody::setRadius
- * @param rayon
+ * @brief CircularBody::setRadius Manipulateur de rayon
+ * @param rayon Valeur du nouveau rayon à assigner à l'instance courante
  */
 void CircularBody::setRadius(double rayon)
 {
     radius = rayon;
 }
+
 /**
- * @brief CircularBody::operator =
- * @param other
- * @return
+ * @brief CircularBody::operator = Copie les valeurs des attributs de other dans l'instance courante
+ * @param other CircularBody à copier
+ * @return L'instance courante après sa copie
  */
 CircularBody& CircularBody::operator=(CircularBody const& other)
 {
     copie(other);
     return (*this);
 }
+
 /**
- * @brief CircularBody::move
- * @param vector
+ * @brief CircularBody::move Ajoute à la position de l'instance courante un vecteur de déplacement (permet de faire bouger le CircularBody)
+ * @param vector Coordonnées du vecteur de déplacement
  */
 void CircularBody::move(const Vec2d& vector)
 {
     position += vector;
 }
 /**
- * @brief CircularBody::contains
- * @param other
- * @return
+ * @brief CircularBody::contains Détermine si le CircularBody en paramètre est contenu dans l'instance courante
+ * @param other CircularBody pour lequel on vérifie s'il est contenu dans l'instance courante
+ * @return true si other est en collision avec l'instance courante et false sinon
  */
 bool CircularBody::contains(const CircularBody& other) const
 {
